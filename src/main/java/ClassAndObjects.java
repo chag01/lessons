ce57cb25f384c788fda6696c9be1e8a8da34827a
chag01 <deniszhuraveldoc@gmail.com>
2019-12-16 23:48:33 +0200

    commit примеры цикла while, if , for добавил персонажа в ClassAndObjects

--- src/main/java/ClassAndObjects.java
+++ src/main/java/ClassAndObjects.java
@@ -3,12 +3,13 @@
 // В java файле public class может быть только один
 // у классе описываются данные (переменные) и операции которые он может совершать(методы)
 // разница класса и объекта - например класс это чертеж, а объект это машина построенная  на основании чертежа.
+
 public class ClassAndObjects {
     public static void main (String [] args){
 
     Person person1 = new Person();      // создание объекта класса Person
-    person1.name="Denis";
-    person1.age=27;
+    person1.name="Timofey";
+    person1.age=22;
     System.out.println("Меня зовут " +person1.name +", мой возраст:" +person1.age);
 
     Person person2 = new Person();      // создание объекта класса Person
@@ -16,6 +17,8 @@ public class ClassAndObjects {
     person2.age=20;
     System.out.println("Меня зовут " +person2.name +", мой возраст:" +person2.age);
 
+    Person si = new Person();
+    si.showInfo("denis", 27);
     }
 }
 
@@ -25,14 +28,7 @@ class Person{
     String name;
     int age;
     public static void showInfo(String name, int age){
-        System.out.println(name+age);
-    }
-    /*run(){
-
+        System.out.println("Меня зовут " +name +", мой возраст:" +age);
     }
 
-    watch(){
-
-    }
-*/
 }
