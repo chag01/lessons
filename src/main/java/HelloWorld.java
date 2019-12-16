7e9a04af09ee270dff8121366ee2f72d2bfed16e
chag01 <deniszhuraveldoc@gmail.com>
2019-12-16 21:03:17 +0200

    изменил HelloWorld + изменил gitignore

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
 
 public class HelloWorld {
-    public static void main (String [] args){
-        System.out.println("Hello world!!!");
+    public static void main(String[] args) {
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
         System.out.println("Hello world3");
     }
 }
