a02a311759569c8090b533b6cfe5e07eeb9354a1
dn190192zdv <denis.zhuravel@privatbank.ua>
2019-12-20 14:00:12 +0200

    changed DoWhile+WhileLoops

--- src/main/java/DoWhile.java
+++ src/main/java/DoWhile.java
@@ -1,9 +1,33 @@
 /**
- *
+ * цикл будет повторяться до тех пока условие = true, если условие false то выходит из цикла
+ * While сначала проверяет условия, а потом выполняет инструкции, а do while сначала выполняет инструкции, а затем выполняет условия.
+ * doWhile гарантирует хотя бы однократное выполнение условия
  */
 
-public class DoWhile {
-    public static void main (String [] args){
+import java.util.Scanner;
 
+
+public class DoWhile {
+    /**
+     * одинаковый метод при использовании while  и do while.
+     * public static void main (String [] args){
+     * Scanner scanner = new Scanner(System.in);
+     * System.out.println("Введите 5");
+     * int value = scanner.nextInt();              // в переменную value подставляем то что ввели с клавиатуры
+     * while (value!=5){
+     * System.out.println("Введите 5");
+     * value = scanner.nextInt();
+     * }
+     * System.out.println("Вы Ввели 5");
+     * }
+     **/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        do {
+            System.out.println("Введите 5");
+            value = scanner.nextInt();
+        } while (value!=5);
+        System.out.println("Вы ввели 5");
     }
 }
