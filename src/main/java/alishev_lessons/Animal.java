efca2f2cbdab93609d636a8c82615dd604ab83f4
dn190192zdv <denis.zhuravel@privatbank.ua>
2020-01-08 16:37:54 +0200

    upcasting+downcasting

--- src/main/java/alishev_lessons/Animal.java
+++ src/main/java/alishev_lessons/Animal.java
@@ -3,6 +3,16 @@ package alishev_lessons;
 public class Animal {
     String name = "Some animal";
 
+    // Upcasting - восходящее преобразование, т.к. класс Dog наследуется от Animal. Восходящее происходит неявно
+    Dog dog = new Dog();
+    Animal animal = new Dog();
+    Animal animal1 = dog; // одно и тоже что и запись выше
+
+    // Downcasting - нисходящее преобразование . Необходимо явно указывать преобразование
+    Dog dog2 = (Dog) animal; // указываем что в  переменной animal лежит объект класса dog
+    // такие преобразования не менятюо объект, они меняют ссылку
+
+
     // создаем 2 метода которыми наделены объекты класса animal
     public void eat(){
         System.out.println("I am eating");
